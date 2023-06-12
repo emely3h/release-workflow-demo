1a37549ee1b15b5599b76f2d3178bd2b7ab30fcb
emely3h <emely3h@gmail.com>
2023-06-12 16:41:43 +0200

    debug

--- update-poetry-version.sh
+++ update-poetry-version.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo('Executing script !!!')
+echo 'Executing script !!!'
 
 # Get the latest tag on the master branch
 latest_tag=$(git describe --tags --abbrev=0)
 
+echo 'fetched latest tag'
+
 # Bump the version using the version bumping tool
 bump2version minor --verbose  # Example: bumping the minor version
+
+echo 'bumped poetry version'
 
 # Push the updated pyproject.toml file
 git push origin main
