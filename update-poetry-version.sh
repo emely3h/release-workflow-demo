677e93a03415777f1c9426860071e30da820c028
emely3h <emely3h@gmail.com>
2023-06-12 16:13:23 +0200

    add workflow

---
+++ update-poetry-version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the latest tag on the master branch
+latest_tag=$(git describe --tags --abbrev=0)
+
+# Bump the version using the version bumping tool
+bump2version minor --verbose  # Example: bumping the minor version
+
+# Push the updated pyproject.toml file
+git push origin master
